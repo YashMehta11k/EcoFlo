5b332122f313ee45bb5c01d511b49df95bf33e8a
Yash P Mehta <yashmht44@gmail.com>
2024-01-26 22:43:57 +0530

    Worked on Express setup and initial transport routes

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import transports from './data/transports.js';
+const port=5000;
+const app=express();
+app.get('/',(req,res)=>{
+    res.send('API is running ....!')
+});
+
+app.get('/api/transports',(req,res)=>{
+    res.json(transports);
+})
+
+app.get('/api/transports/:APPS',(req,res)=>{
+    const transport=transports.find((p)=>p.APPS===req.params.APPS);
+    res.json(transport);
+})
+
+app.listen(port,()=>console.log(`Server running on port ${port}`))
